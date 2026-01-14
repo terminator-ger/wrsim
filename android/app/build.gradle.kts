4a7ca82919884e2ca866ed481bd3c05fa58ded8b
ml <michael.lechner.trash@t-online.de>
2026-01-14 09:20:52 +0100

    fix gpt bullshit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,6 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-            shrinkResources false
-            minifyEnabled false
         }
     }
 }
