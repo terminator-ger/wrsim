f5cf0faae7344b0278e4938ab508f1fd7861c91b
ml <michael.lechner.trash@t-online.de>
2026-01-21 14:45:38 +0100

    vercode

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.Properties
 import java.io.FileInputStream
 
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import com.android.build.api.variant.FilterConfiguration
+import com.android.build.api.variant.FilterConfiguration.FilterType.*
 
 plugins {
     id("com.android.application")
@@ -67,18 +67,17 @@ flutter {
 }
 
 
-dependencies {}
-
-/ Map for the version code that gives each ABI a value.
+// Map for the version code that gives each ABI a value.
 val abiCodes = mapOf("armeabi-v7a" to 1, "x86" to 2, "x86_64" to 3)
 
 // For per-density APKs, create a similar map:
 // val densityCodes = mapOf("mdpi" to 1, "hdpi" to 2, "xhdpi" to 3)
 
-import com.android.build.api.variant.FilterConfiguration.FilterType.*
+
 
 // For each APK output variant, override versionCode with a combination of
-// abiCodes * 1000 + variant.versionCode. In this example, variant.versionCode
+// variant.versioncode * 1000 + abicode
+// abiCodes * 10 + variant.versionCode. In this example, variant.versionCode
 // is equal to defaultConfig.versionCode. If you configure product flavors that
 // define their own versionCode, variant.versionCode uses that value instead.
 androidComponents {
@@ -98,7 +97,7 @@ androidComponents {
             if (baseAbiCode != null) {
                 // Assigns the new version code to output.versionCode, which changes the version code
                 // for only the output APK, not for the variant itself.
-                output.versionCode.set(baseAbiCode * 1000 + (output.versionCode.get() ?: 0))
+                output.versionCode.set(10 * output.versionCode.get() + baseAbiCode)
             }
         }
     }
